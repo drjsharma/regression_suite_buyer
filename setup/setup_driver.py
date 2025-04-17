86f4b4f9eee50d21016de4ada2b0f3b5190cf5bd
drjsharma <109914828+drjsharma@users.noreply.github.com>
2025-04-17 13:33:50 +0530

    Update setup_driver.py

--- setup/setup_driver.py
+++ setup/setup_driver.py
@@ -1,13 +1,15 @@
-import pytest
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
 
 @pytest.fixture(scope="class")
 def setup():
-    """ ✅ Fixture: Initialize and Quit WebDriver Automatically """
+    options = Options()
+    options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+
     service = Service(ChromeDriverManager().install())
-    driver = webdriver.Chrome(service=service)
+    driver = webdriver.Chrome(service=service, options=options)
     driver.maximize_window()
-    yield driver  # Return driver instance
+    yield driver
     driver.quit()
+
